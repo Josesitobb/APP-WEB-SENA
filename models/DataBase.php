a7ae392611b7607849b68c18f8b20e73b34acdaf
Josesitobb <124468479+Josesitobb@users.noreply.github.com>
2023-10-15 08:48:59 -0500

    Hola mundo

--- models/DataBase.php
+++ models/DataBase.php
@@ -3,13 +3,13 @@
         
         # Conexión Externa
         public static function connection(){            
-            $hostname = "senajose.mysql.database.azure.com";
+            $hostname = "dbjosesena.mysql.database.azure.com";
             $port = "3306";
             $database = "dbejemplouser";
-            $username = "Josesena";
+            $username = "Josegamendb";
             $password = "Nicoll123";
             $options = array(
-                PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootG2.crt.pem'
+                PDO::MYSQL_ATTR_SSL_CA =>'assets/docs/DigiCertGlobalRootCA.crt.pem'
             );
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
